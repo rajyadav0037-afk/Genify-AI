e89bbbb169fa98ec955db7c0d8532bdd0b0f3603
rajyadav0037-afk <raj.yadav0037@gmail.com>
2026-04-18 01:54:21 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -6,7 +6,12 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("DB Connected"));
