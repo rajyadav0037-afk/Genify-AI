97333388b753e55d8a34d66abaa47b01f68d636f
rajyadav0037-afk <raj.yadav0037@gmail.com>
2026-04-18 19:46:38 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(cors({
+  origin: "*"
+}));
+
+app.options("*", (req, res) => {
+  res.sendStatus(200);
+});
+
 app.use(cors(...));
 app.options("*", cors());
 
