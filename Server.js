83b0c9b3be3093d7096ab586c58f182e3262a82c
rajyadav0037-afk <raj.yadav0037@gmail.com>
2026-04-18 19:23:28 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+app.options("*", cors());
+
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("DB Connected"));
 
