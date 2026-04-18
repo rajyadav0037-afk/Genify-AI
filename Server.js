cbab7573b2bc04d1c567e9c7a3a5f312ba21aa7c
rajyadav0037-afk <raj.yadav0037@gmail.com>
2026-04-18 19:27:37 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(cors(...));
+app.options("*", cors());
+
 origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
